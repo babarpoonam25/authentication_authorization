ad259807dc274333ee8ac57d754ff82bf3f0f461
poonambabarapply <poonambabarapply@gmail.com>
2024-04-03 10:17:31 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
+// app.js
+const express = require("express");
 const mysql = require("mysql");
+const apiRouter = require("./api");
+
+const app = express();
 
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
+  password: "databaseOP",
   database: "student_DB",
 });
 
 connection.connect((err) => {
   if (err) {
-    console.log("error connecting to database", err);
-    return;
+    console.log("Error connecting to Database:", err);
+  } else {
+    console.log("Connected to MySQL database");
   }
-  console.log("connected to MYSQL database!!!");
 });
+
+app.use("/api", apiRouter);
+
+// Error handling middleware for unauthorized access
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: "Unauthorized access" });
+  }
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
 module.exports = connection;
